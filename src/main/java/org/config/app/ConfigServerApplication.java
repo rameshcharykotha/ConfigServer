cafb66b61f55f6cd5414f5528bdb48d5bb40f75a
rameshcharykotha <rameshcharykotha@gmail.com>
2024-07-20 21:43:57 -0500

    Updated application.yml

--- src/main/java/org/config/app/ConfigServerApplication.java
+++ src/main/java/org/config/app/ConfigServerApplication.java
@@ -1,4 +1,13 @@
 package org.config.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
